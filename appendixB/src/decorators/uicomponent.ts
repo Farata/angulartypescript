99b6e327a8f72609b0be0a561ce6635c33a9b0bf
Yakov Fain <yakovfain@gmail.com>
2017-08-17 10:01:17 -0400

    First commit

---
+++ appendixB/src/decorators/uicomponent.ts
@@ -0,0 +1,13 @@
+function UIcomponent(html: string) {
+    console.log(`The decorator received ${html} \n`);
+
+    return function(target){
+        console.log(`Creating a UI component from \n ${target} ` );
+    };
+}
+
+@UIcomponent("<h1>Hello Shopper!</h1>")
+class Shopper {
+
+    constructor(private name: string) {}
+}
---
+++ appendixB/src/let-var.ts
@@ -0,0 +1,10 @@
+(function (){
+
+        if (true) {
+         let customer = "Mary";
+         console.log("The name of the customer inside the block is "  + customer);
+        }
+
+         //console.log("The name of the customer outside the block is "  + customer);
+
+    })();
