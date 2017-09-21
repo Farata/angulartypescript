210ea4fcc8a320ff2d13606c016fced45e1707c5
Yakov Fain <yakovfain@gmail.com>
2017-09-21 10:58:37 -0400

    updated the resolver sample

--- chapter3/router-samples/src/app/resolver/data.resolver.ts
+++ chapter3/router-samples/src/app/resolver/data.resolver.ts
@@ -1,13 +1,14 @@
-import {Resolve, ActivatedRouteSnapshot} from "@angular/router";
+import {Resolve} from "@angular/router";
 import {DataService} from "./data.service";
 import {Injectable} from "@angular/core";
+import { Observable} from "rxjs/Observable";
 
 @Injectable()
-export class DataResolver implements Resolve<any>{
+export class DataResolver implements Resolve<string[]>{
 
     constructor ( private dataService: DataService){}
 
-    resolve(route: ActivatedRouteSnapshot){
+    resolve(): Observable<string[]>{
         return this.dataService.loadData();
     }
 }
