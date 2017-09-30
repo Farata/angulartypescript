62ef0131c0c2e582a2beb569880c3507698eed77
Yakov Fain <yakovfain@gmail.com>
2017-09-30 13:59:45 -0400

    updated flex-layout samples

--- chapter5/observables/src/app/flex-layout/app.component.ts
+++ chapter5/observables/src/app/flex-layout/app.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
     .left   { background-color: cyan; }
     .right  { background-color: yellow; }
   `],
-  template: `
-    <div fxLayout="row" fxLayout.sm="column" class="parent">
+  template: ` 
+    <div fxLayout="row" class="parent" fxLayout.sm="column" >
       <div fxFlex="30%" class="left">Left</div>
       <div fxFlex="70%" class="right">Right</div>
     </div>
--- chapter5/observables/src/app/observable-media/app.component.ts
+++ chapter5/observables/src/app/observable-media/app.component.ts
@@ -3,12 +3,20 @@ import { ObservableMedia } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-root',
-  template: '<h3>See breakpoint activation messages in the console.</h3>'
+  template: `<h3>Watch the breakpoint activation messages in the console.</h3>
+  <span *ngIf="showExtras">Showing extra info on large screens</span>
+    
+  `
 })
 export class AppComponent {
+  showExtras = false;
+
   constructor(private media: ObservableMedia) {
     this.media.asObservable().subscribe(mediaChange => {
       console.log(mediaChange.mqAlias);
+
+      this.showExtras = mediaChange.mqAlias==='lg'? true:false;
+
     });
   }
 }
