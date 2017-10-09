b41bcf6c1a5e9c7668d8689bd04cfde415f30ad1
Yakov Fain <yakovfain@gmail.com>
2017-10-09 11:02:07 -0400

    added ngAuction code

---
+++ chapter6/ng-auction/src/app/product/product.component.ts
@@ -0,0 +1,29 @@
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Product, ProductService } from '../shared/services';
+
+@Component({
+  selector: 'nga-product',
+  styleUrls: [ './product.component.scss' ],
+  templateUrl: './product.component.html'
+})
+export class ProductComponent {
+  product$: Observable<Product>;
+  suggestedProducts$: Observable<Product[]>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {
+    this.product$ = this.route.paramMap
+      .map(params => parseInt(params.get('productId') || '', 10))
+      .filter(productId => !!productId)
+      .switchMap(productId => this.productService.getById(productId));
+
+    this.suggestedProducts$ = this.productService.getAll();
+  }
+}
