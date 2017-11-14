9d7ee923cd1de00016817d470244bca13c26e749
Yakov Fain <yakovfain@gmail.com>
2017-11-14 13:10:58 -0500

    upgraded to Angular 5

--- chapter6/inter-component/src/app/mediator-parent/order.component.ts
+++ chapter6/inter-component/src/app/mediator-parent/order.component.ts
@@ -10,18 +10,12 @@ export class OrderComponent {
 
   message = "Waiting for orders...";
 
-  private _stock: IStock;
-
   @Input() set stock(value: IStock ){
     if (value && value.bidPrice != undefined) {
       this.message = `Placed order to buy 100 shares
                           of ${value.stockSymbol} 
                           at \$${value.bidPrice.toFixed(2)}`;
     }
-  }
-
-  get stock(): IStock{
-    return this._stock;
   }
 }
 
