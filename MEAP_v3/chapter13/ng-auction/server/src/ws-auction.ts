16121ef8bea02dbcf3186d19c870c3315f7e784d
Anton Moiseev <a@antonmoiseev.com>
2018-01-17 18:42:14 +0400

    ch13: Get rid of user IDs on the ngAuction server

--- MEAP_v3/chapter13/ng-auction/server/src/ws-auction.ts
+++ MEAP_v3/chapter13/ng-auction/server/src/ws-auction.ts
@@ -1,10 +1,6 @@
 import * as http from 'http';
 import * as ws from 'ws';
-import { parse as parseUrl } from 'url';
-import { parse as parseQueryString } from 'querystring';
 import { updateProductBidAmount} from './db-auction';
-
-type UserId = string;
 
 interface BidMessage {
   productId: number;
@@ -17,69 +13,35 @@ export function createBidServer(httpServer: http.Server): BidServer {
 
 export class BidServer {
   private readonly wsServer: ws.Server;
-  private readonly productsToUsers = new Map<number, Set<UserId>>();
-  private readonly usersToConnections = new Map<UserId, Set<ws>>();
 
   constructor(server: http.Server) {
     this.wsServer = new ws.Server({ server });
-    this.wsServer.on('connection',
-       (userSocket: ws, httpRequest: http.IncomingMessage) => this.onConnection(userSocket, httpRequest));
+    this.wsServer.on('connection', (userSocket: ws) => this.onConnection(userSocket));
   }
 
-  private onConnection(ws: ws, req: http.IncomingMessage): void {
-    // Remember who is associated with current WS connection,
-    // so we can send bid updates to specific users.
-    const userId = BidServer.parseUserId(req);
-    const connections = this.usersToConnections.get(userId) || new Set<ws>();
-    this.usersToConnections.set(userId, connections.add(ws));
-
+  private onConnection(ws: ws): void {
     // Subscribe to WebSocket events.
+    ws.on('message', (message: string) => this.onMessage(message));
+    ws.on('error', (error: Error) => this.onError(error));
+    ws.on('close', () => this.onClose());
 
-    ws.on('message', (message: string) => this.onMessage(userId, message));
-    ws.on('close', (userSocket: ws) => this.onClose(userId, userSocket));
-    ws.on('error',
-     (userSocket: ws) => this.onError(userId, userSocket));
-
-    console.log(`
-      Connection opened.
-      Open connections count: ${this.wsServer.clients.size} - ${connections.size}
-    `);
+    console.log(`Connections count: ${this.wsServer.clients.size}`);
   }
 
-  private onMessage(userId: UserId, message: string): void {
+  private onMessage(message: string): void {
     const bid: BidMessage = JSON.parse(message);
-    const subscribedUsers = this.productsToUsers.get(bid.productId);
-    this.productsToUsers.set(bid.productId, subscribedUsers.add(userId));
     updateProductBidAmount(bid.productId, bid.amount);
 
     // Broadcast the new bid amount
-    Array.from(subscribedUsers)
-      .map(userId => this.usersToConnections.get(userId))
-      .reduce((allConnections: ws[], userConnections: Set<ws>) => {
-        return allConnections.concat(Array.from(userConnections));
-      }, [])
-      .forEach(ws => ws.send(JSON.stringify(bid)));
-
-    console.log(this.productsToUsers);
+    this.wsServer.clients.forEach(ws => ws.send(JSON.stringify(bid)));
+    console.log(`Bid ${bid.amount} is placed on product ${bid.productId}`);
   }
 
-  private onClose(userId: UserId, ws: ws): void {
-    const connections = this.usersToConnections.get(userId);
-    connections.delete(ws);
-
-    console.log(`Connection closed. Open connections count: ${this.wsServer.clients.size} - ${connections.size}`);
+  private onClose(): void {
+    console.log(`Connections count: ${this.wsServer.clients.size}`);
   }
 
-  private onError(userId: UserId, ws: ws): void {
-    //console.error(`WebSocket error. User ID: ${userId}. Error message: "${error.message}"`);
-    console.error(`WebSocket error. User ID: ${userId}. Error message: "${ws['message']}"`);
-    const connections = this.usersToConnections.get(userId);
-    connections.delete(ws);
-  }
-
-  private static parseUserId(req: http.IncomingMessage): UserId {
-    const url = parseUrl(req.url || '');
-    const params = parseQueryString(url.query || '');
-    return params.userId as string;
+  private onError(error: Error): void {
+    console.error(`WebSocket error: "${error.message}"`);
   }
 }
