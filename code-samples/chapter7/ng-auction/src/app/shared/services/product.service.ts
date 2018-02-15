0bd0fc2612099c845455ba816da8d4dfeb89df24
Yakov Fain <yakovfain@gmail.com>
2018-02-15 09:04:28 -0500

    code refactoring

--- code-samples/chapter7/ng-auction/src/app/shared/services/product.service.ts
+++ code-samples/chapter7/ng-auction/src/app/shared/services/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 export interface Product {
   id: number;
