072d6df1667ee66898ce41bcc23bf6edd2b0a6d3
Yakov Fain <yakovfain@gmail.com>
2018-02-19 14:42:08 -0500

    added first ngrx sample

---
+++ code-samples/chapter15/counter/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {select, Store} from "@ngrx/store";
+import {INCREMENT, DECREMENT} from "./reducer";
+
+@Component({
+  selector: 'app-root',
+  template: `    
+     <button (click)="increment()">Increment</button>
+     <button (click)="decrement()">Decrement</button>
+     <p>The counter: {{counter$ | async}}</p>
+     <app-child></app-child>
+  `
+})
+export class AppComponent {
+  counter$: Observable<number>;
+
+  constructor(private store: Store<any>) {
+    this.counter$ = store.pipe(select('counterState'));
+    // this.counter$ = store.pipe(map((stValue) => stValue.counte));
+  }
+
+  increment(){
+    this.store.dispatch({type: INCREMENT});
+  }
+
+  decrement(){
+    this.store.dispatch({type: DECREMENT});
+  }
+}
---
+++ code-samples/chapter15/counter/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import {counterReducer} from "./reducer";
+import {StoreModule} from "@ngrx/store";
+import { ChildComponent } from './child/child.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChildComponent
+  ],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({counterState: counterReducer})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
