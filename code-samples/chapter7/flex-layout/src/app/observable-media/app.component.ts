076b524f3736c56e3699331b02d9e7f09a50d90a
Yakov Fain <yakovfain@gmail.com>
2018-03-10 10:14:01 -0500

    updated code samples in ch7

--- code-samples/chapter7/flex-layout/src/app/observable-media/app.component.ts
+++ code-samples/chapter7/flex-layout/src/app/observable-media/app.component.ts
@@ -6,15 +6,15 @@ import {map} from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   template: `<h3>Watch the breakpoint activation messages in the console.</h3>
-  <span *ngIf="showExtras | async">Showing extra info on medium screens</span>
+  <span *ngIf="showExtras$ | async">Showing extra info on medium screens</span>
     
   `
 })
 export class AppComponent {
-  showExtras: Observable<boolean>;
+  showExtras$: Observable<boolean>;
 
   constructor(private media: ObservableMedia) {
-    this.showExtras = this.media.asObservable()
+    this.showExtras$ = this.media.asObservable()
       .pipe(map(mediaChange => {
            console.log(mediaChange.mqAlias);
            return mediaChange.mqAlias === 'md'? true: false;
