12d41f720504830dcc031f2bc7fce691ae7a92da
Yakov Fain <yakovfain@gmail.com>
2018-03-22 22:16:25 -0400

    removed build dirs

--- code-samples/chapter4/router-advanced-samples/src/app/resolver/data.resolver.ts
+++ code-samples/chapter4/router-advanced-samples/src/app/resolver/data.resolver.ts
@@ -1,5 +1,4 @@
 import {Resolve} from "@angular/router";
-import {DataService} from "./data.service";
 import {Injectable} from "@angular/core";
 import { Observable} from "rxjs/Observable";
 import {HttpClient} from "@angular/common/http";
