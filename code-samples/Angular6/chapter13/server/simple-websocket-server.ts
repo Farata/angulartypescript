59ad0a30cba42dda1ca8e1a3831ef487a8ebad41
Yakov Fain <yakovfain@gmail.com>
2018-06-13 22:40:37 -0400

    simplified the server code

--- code-samples/Angular6/chapter13/server/simple-websocket-server.ts
+++ code-samples/Angular6/chapter13/server/simple-websocket-server.ts
@@ -9,13 +9,12 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../simple-websocket-client.html')));
 
 const httpServer = app.listen(8000, "localhost", () => {
-    const {port} = httpServer.address();
-    console.log(`HTTP server is listening on ${port}`);
+    console.log(`HTTP server is listening on localhost:8000`);
 });
 
 // WebSocket Server
 const wsServer = new Server({port:8085});
-console.log('WebSocket server is listening on port 8085');
+console.log('WebSocket server is listening on localhost:8085');
 
 wsServer.on('connection',
            wsClient => {
@@ -25,6 +24,10 @@ wsServer.on('connection',
                    console.log(`The server received: ${error['code']}`);
            }
 );
+
+
+
+
 
 // Broadcasting to all clients
 /*
