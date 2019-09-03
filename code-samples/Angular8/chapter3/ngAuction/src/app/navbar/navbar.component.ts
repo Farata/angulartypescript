fd85e0f3d7dce4b1db7665a234c5304a546c7c69
Yakov Fain <yakovfain@gmail.com>
2019-09-03 12:43:11 -0400

    Migrating router sample from ch3

---
+++ code-samples/Angular8/chapter3/ngAuction/src/app/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'nga-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent  {
+
+}
